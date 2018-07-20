174fb4b81fe84d00513afbe2bdd8ce8c15f05516
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-07-20 17:06:02 +0200

    Add index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import createHistory from 'history/createBrowserHistory';
+import { ConnectedRouter } from 'react-router-redux';
+import { injectGlobal } from 'styled-components';
+import WebFont from 'webfontloader';
+import webFontConfig from './configs/webfonts';
+import Routes from './views/routes';
+import configureStore from './state';
+import colors from './views/constants/colors';
+
+/* eslint-disable no-unused-expressions */
+injectGlobal`
+  body {
+    margin: 0;
+    background: ${colors.background};
+  }
+  * {
+    box-sizing: border-box;
+  }
+  #app {
+    min-height: 100%
+  }
+`;
+/* eslint-enable no-unused-expressions */
+
+const store = configureStore();
+const history = createHistory();
+
+WebFont.load(webFontConfig);
+
+render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <Routes />
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('app')
+);
