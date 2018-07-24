78666503629d8385b4a9d5aaf24e3814c94e2ebb
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-07-24 09:55:28 +0200

    Add color constants file

---
+++ views/constants/colors.js
@@ -0,0 +1,11 @@
+const colors = {
+  primary: 'white',
+  secondary: '#001529',
+  dark: 'gray',
+  light: '#fff',
+  gray: '#f0f2f5',
+  background: '#f0f2f5',
+  error: 'red'
+};
+
+export default colors;
