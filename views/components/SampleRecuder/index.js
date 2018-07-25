b6c999622dc7e89db95a9f533d9ef5cd2fcdaa5b
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-07-25 02:47:08 +0200

    Add regular components

---
+++ views/components/SampleRecuder/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import FlexBox from '../../common/FlexBox';
+
+const SampleReducer = () => (
+  <FlexBox align="center" justify="space-around">
+    <p>Sample Reducer:</p>
+    <Button variant="contained" color="primary">
+      Primary
+    </Button>
+  </FlexBox>
+);
+
+export default SampleReducer;
