f45d16733d99695756bc307f6045497f836d133b
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-07-25 15:47:00 +0200

    Add container components

---
+++ views/containers/SampleReducerContainer/index.js
@@ -0,0 +1,20 @@
+import { connect } from 'react-redux';
+
+import SampleReducer from '../../components/SampleRecuder';
+import { getPingRequest } from '../../../state/ping/actions';
+import { getPingMessage, pingHasError, pingIsLoading } from '../../../state/ping/selectors';
+
+const mapDispatchToProps = dispatch => ({
+  fetchPing: () => dispatch(getPingRequest())
+});
+
+const mapStateToProps = state => ({
+  error: pingHasError(state),
+  loading: pingIsLoading(state),
+  pingMessage: getPingMessage(state)
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SampleReducer);
