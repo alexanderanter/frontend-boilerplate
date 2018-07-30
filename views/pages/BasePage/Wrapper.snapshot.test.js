af57eecdef7ace80cd2156f7b43a286274625f92
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-07-30 15:12:41 +0200

    Add basic tests for BasePage

---
+++ views/pages/BasePage/Wrapper.snapshot.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import 'jest-styled-components';
+import Wrapper from './Wrapper';
+
+describe('Wrapper', () => {
+  let props;
+  let wrapper;
+
+  const Container = () => {
+    if (!wrapper) {
+      wrapper = shallow(<Wrapper {...props}>{props.children}</Wrapper>);
+    }
+    return wrapper;
+  };
+
+  // Reset Component and Props before each test.
+  beforeEach(() => {
+    props = {};
+    wrapper = undefined;
+  });
+
+  describe('without props', () => {
+    it('Should match snapshot', () => {
+      expect(toJson(Container())).toMatchSnapshot();
+    });
+  });
+});
