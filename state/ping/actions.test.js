8bc9aeb0a1bd431e5f008d1b4cd92265758dae6a
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-07-31 11:08:37 +0200

    Init basic action test

---
+++ state/ping/actions.test.js
@@ -0,0 +1,41 @@
+import { apiMiddleware, ApiError } from 'redux-api-middleware';
+import configureMockStore from 'redux-mock-store';
+import thunkMiddleware from 'redux-thunk';
+import fetchMock from 'fetch-mock';
+import { getPingRequest } from './actions';
+import types from './types';
+
+const mockStore = configureMockStore([thunkMiddleware, apiMiddleware]);
+
+// describe unit
+describe('getPingRequest() async action creator', () => {
+  afterEach(() => {
+    fetchMock.restore();
+  });
+
+  it('dispatches FETCH_PING_FAILURE action on failed request', done => {
+    const apiResponse = {
+      message: 'Unexpected token <'
+    };
+
+    fetchMock('http://localhost:8080/ping', 500, apiResponse);
+
+    const expectedActions = [
+      {
+        meta: undefined,
+        payload: undefined,
+        type: types.FETCH_PING_REQUEST
+      },
+      {
+        error: true,
+        meta: undefined,
+        payload: new ApiError(500, 'Internal Server Error', apiResponse),
+        type: types.FETCH_PING_FAILURE
+      }
+    ];
+
+    const store = mockStore({}, expectedActions, done);
+
+    store.dispatch(getPingRequest());
+  });
+});
