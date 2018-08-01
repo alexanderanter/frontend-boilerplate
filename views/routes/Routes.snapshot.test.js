9ba7e23a0c89d267d51fe34bfe8a356085e38a57
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-08-01 13:40:17 +0200

    Add Router test

---
+++ views/routes/Routes.snapshot.test.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import 'jest-styled-components';
+import { MemoryRouter } from 'react-router-dom';
+import configureMockStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
+import Routes from './index';
+
+describe('Routes', () => {
+  let props;
+  let wrapper;
+  let initialEntries;
+  const initialState = {
+    ping: {
+      pingData: {
+        message: 'Pong!',
+        error: 'error',
+        loading: true
+      }
+    }
+  };
+  const mockStore = configureMockStore();
+  const store = mockStore(initialState);
+
+  const Wrapper = () => {
+    if (!wrapper) {
+      wrapper = mount(
+        <Provider store={store}>
+          <MemoryRouter initialEntries={initialEntries}>
+            <Routes {...props}>{props.children}</Routes>
+          </MemoryRouter>
+        </Provider>
+      );
+    }
+    return wrapper;
+  };
+
+  // Reset Component and Props before each test.
+  beforeEach(() => {
+    props = {};
+    initialEntries = [];
+    wrapper = undefined;
+  });
+
+  describe('on / route', () => {
+    it('should match snapshot for HomePage', () => {
+      initialEntries = ['/'];
+      expect(toJson(Wrapper())).toMatchSnapshot();
+    });
+  });
+});
