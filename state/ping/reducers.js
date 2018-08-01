059aed80aacec1dc047cc2501f7340a5480f7157
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-08-01 10:55:55 +0200

    Remove error object

--- state/ping/reducers.js
+++ state/ping/reducers.js
@@ -10,13 +10,12 @@ const pingReducer = (state = INITIAL_STATE, action) => {
     case types.FETCH_PING_REQUEST:
       return { ...state, pingData: { message: '', error: null, loading: true } };
     case types.FETCH_PING_SUCCESS:
-      console.log(action);
       return {
         ...state,
         pingData: { message: action.payload.message, error: null, loading: false }
       };
     case types.FETCH_PING_FAILED:
-      error = action.payload.error || { message: action.payload.message };
+      error = action.payload.error || action.payload.message;
       return { ...state, pingData: { message: '', error, loading: false } };
     default:
       return state;
