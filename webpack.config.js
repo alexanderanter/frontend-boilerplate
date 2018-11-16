59fd4a28901b7074008cf57a95169a2ce2326659
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-11-16 06:59:36 +0100

    Add styled components tooling

--- webpack.config.js
+++ webpack.config.js
@@ -31,11 +31,7 @@ module.exports = {
         test: /\.jsx?$/,
         include: path.join(__dirname, '/'),
         exclude: /(node_modules|bower_compontents)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['@babel/preset-env', '@babel/preset-react'],
-          plugins: ['@babel/plugin-proposal-class-properties']
-        }
+        loader: 'babel-loader'
       },
       {
         test: /\.css$/,
